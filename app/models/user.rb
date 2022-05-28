2501f2eb26f9740240255e83d0ca4378e9eaa397
ikael <ikael@student.21-school.ru>
2022-05-28 14:27:11 +0300

    Advanced login
    implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
+  attr_accessor :remember_token
+
+  before_save { self.email = email.downcase }
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -18,5 +20,25 @@ class User < ApplicationRecord
       BCrypt::Engine.cost
     end
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # returns a random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # returns true of the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:spider)
   end
 
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # log in to set the cookie
+    log_in_as(@user, remember_me: '1')
+    #log in again and verify that the cookie is deleted
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
+  end
+
   test "login with valid information followeb by logout" do
     get login_path
     post login_path, params: { session: { email: @user.email,
@@ -21,6 +34,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+
+    # simulate a user clicking logout in a second window
+    delete logout_path
+
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
